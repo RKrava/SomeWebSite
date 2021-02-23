9a2cf99b5681586c3710c1d3ce1a73f96d58ede2
Roma <krab9887@gmail.com>
2021-02-23 18:05:37 +0200

    added WebSite

---
+++ WebSite/WebSite/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebSite.Models;
+using WebSite.Services;
+
+namespace WebSite.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; set; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get(
+            [FromQuery] string ProductId,
+            [FromQuery] int Rating)
+        {
+            ProductService.AddRating(ProductId,Rating);
+            return Ok();
+        }
+    }
+}
